684dbbdee61d570c318869066cc14ca44deeaf8c
NicoloCammarata <nicolo.camma@gmail.com>
2023-12-02 18:43:58 +0100

    password-generator

---
+++ javascripts.js
@@ -0,0 +1,30 @@
+
+// nome //
+const userName = prompt("Dimmi il tuo nome")
+console.log(userName)
+
+
+
+console.log("uderName:", userName, typeof userName);
+
+// cognome //
+
+const userSurname = prompt("Dimmi il tuo Cognome")
+
+console.log("userSurname:", userSurname, typeof userSurname);
+
+// colore //
+
+const favouriteColor = prompt("Dimmi il tuo colore preferito")
+
+console.log("favouriteColor:", favouriteColor, favouriteColor);
+
+
+
+
+
+console.log("userName:" + " " + userName + ", " + "userSurname:" + " " + userSurname + ", " + "favouriteColor:" + " " + favouriteColor);
+
+const presentazione = "userName:" + " " + userName + ", " + "userSurname:" + " " + userSurname + ", " + "favouriteColor:" + " " + favouriteColor + " " + "114";
+document.writeln(presentazione)
+
